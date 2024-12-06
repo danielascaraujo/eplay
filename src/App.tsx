01b3dea5cfc76c6a934e291d06c61b7956415d07
Daniela Araujo <daniela.scaraujo@outlook.com>
2024-12-06 14:06:48 -0300

    construção das páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
-import React from 'react'
-
-import Banner from './components/Banner'
 import Header from './components/Header'
 import { GlobalCss } from './styles'
-import Product from './components/Product'
-import ProductList from './components/ProductList'
 
-const route = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <>
-        <Banner />
-        <ProductList title="Promoções" background="gray" />
-        <ProductList title="Em breve" background="black" />
-      </>
-    )
-  }
-])
+import Rotas from './routes'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalCss />
       <div className="container">
         <Header />
       </div>
-      <RouterProvider router={route} />
-    </>
+      <Rotas />
+    </BrowserRouter>
   )
 }
 
