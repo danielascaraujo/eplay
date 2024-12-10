659b28420e7624cd0cd938fd18fc0b7446441647
Daniela Araujo <daniela.scaraujo@outlook.com>
2024-12-10 15:03:52 -0300

    código da terceira parte

--- src/styles.ts
+++ src/styles.ts
@@ -1,11 +1,16 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const colors = {
-  white: '#EEEEEE',
+  white: '#EEE',
   black: '#111',
   gray: '#333',
   green: '#10AC84',
   lightGray: '#A3A3A3'
+}
+
+export const breakpoints = {
+  desktop: '1024px',
+  tablet: '768px'
 }
 
 export const GlobalCss = createGlobalStyle`
@@ -26,5 +31,9 @@ export const GlobalCss = createGlobalStyle`
     max-width: 1024px;
     width: 100%;
     margin: 0 auto;
+
+        @media (max-width: ${breakpoints.desktop}) {
+      max-width: 80%;
+    }
   }
 `
